44c852e61d004faa1f6f47e76110491f40e7fbed
Romário Costa de Souza <romariocdz@users.noreply.github.com>
2022-07-13 11:43:47 -0300

    js novos exercicios
    novos exercicios de java script

---
+++ exercicios-proprio/exercicio 07/ex07.1/ex08/condiçao4.js
@@ -0,0 +1,12 @@
+var hoje = new Date()
+    var date = hoje.getDate()
+        switch(date) {
+            case(date = 13):
+                console.log('Hoje é quarta feira')
+                    break
+                        case(date = 11):
+                            console.log('hoje é terça')
+                        break   
+                    default:
+                        console.loge('data invalida')
+}

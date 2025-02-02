420a3d8f5fd8113d7514c1723b70caec5ab9e359
Romário Costa de Souza <romariocdz@users.noreply.github.com>
2025-02-02 10:40:32 -0300

    passos

--- exercicios-proprio/while and for/intervalo.js
+++ exercicios-proprio/while and for/intervalo.js
@@ -1,8 +1,10 @@
 number1 = 1;
 number2 = 5;
+inter = 0
 cont = number1;
 soma = 0
 for(cont; cont < number2 ; cont++) {
-    soma = soma + cont
+    inter += cont
+    soma = inter
 }
 console.log(soma)
